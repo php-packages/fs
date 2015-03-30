bea16a59f29cbf7eec5d90c7d0e9d345f6961059
bound1ess <bound1ess.dev@gmail.com>
2015-03-30 20:42:44 +0600

    Write #full method.

--- src/PhpPackages/Fs/Path.php
+++ src/PhpPackages/Fs/Path.php
@@ -42,13 +42,30 @@ class Path {
 
     /**
      * @param string $path
-     * @return Path
+     * @return object
      */
     public function join($path)
     {
         $lastChar = substr($this->path, strlen($this->path) - 1);
 
         $this->path .= (ds() == $lastChar) ? $path : (ds() . $path);
+
+        return $this;
+    }
+
+    /**
+     * @param string|null $parentDir
+     * @return object
+     */
+    public function full($parentDir = null)
+    {
+        $firstChar = substr($this->path, 0, 1);
+
+        if (ds() != $firstChar) {
+            $parentDir = $parentDir ?: getcwd();
+
+            $this->path = (new static($parentDir))->join($this->path)->path();
+        }
 
         return $this;
     }
