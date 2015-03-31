ab2de6c90aa365fa03598b6f31eeace3259c4a4f
bound1ess <bound1ess.dev@gmail.com>
2015-03-31 17:49:37 +0600

    Initialize iterator in Dir#__construct.

--- src/PhpPackages/Fs/Dir.php
+++ src/PhpPackages/Fs/Dir.php
@@ -1,3 +1,23 @@
 <?php namespace PhpPackages\Fs;
 
-class Dir extends Path {}
+use RecursiveDirectoryIterator,
+    FilesystemIterator,
+    RecursiveIteratorIterator; // Cool naming.
+
+class Dir extends Path {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($path)
+    {
+        parent::__construct($path);
+
+        if ($this->isDir()) {
+            $this->iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::CHILD_FIRST
+            );
+        }
+    }
+}
